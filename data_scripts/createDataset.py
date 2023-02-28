6f5b07e9871493825b7809146fb41fbe40daa289
Lucas Mayall <lucasmmayall@gmail.com>
2023-02-27 17:59:52 -0800

    Finished basic createDataset.py, still need to figure out best way to deal with multiple files

--- data_scripts/createDataset.py
+++ data_scripts/createDataset.py
@@ -1,12 +1,20 @@
 import sys
 import os
+from numpy import frombuffer
 import pandas as pd
 import github as gh
+from github.GitCommit import GitCommit
+from github.GitAuthor import GitAuthor
+from github.Commit import Commit
+from github.File import File
+
 from collections import namedtuple
 from dataclasses import dataclass
+import git
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
 
+basePath = os.path.abspath(os.path.join(sys.path[0], ".."))
+    
 DataFields = (
     # boolean comparison
     "sameAuthor",
@@ -23,7 +31,7 @@ DataFields = (
     "bug_authorOwnsRepo"
 )
 
-def getUserData(namePrefix, uObj, row):
+def getGithubUserData(namePrefix, uObj, row):
     row[namePrefix + "_collaborators"] = uObj.collaborators
     row[namePrefix + "_contributions"] = uObj.contributions
     row[namePrefix + "_disk_usage"] = uObj.disk_usage
@@ -33,105 +41,104 @@ def getUserData(namePrefix, uObj, row):
     row[namePrefix + "_hireable"] = uObj.hireable
     row[namePrefix + "_site_admin"] = uObj.site_admin
 
-def getGitUserData(namePrefix, uObj, row):
-    row[namePrefix + "_"] = uObj.collaborators
-    row[namePrefix + "_contributions"] = uObj.contributions
+def getGitUserData(namePrefix, uObj:GitAuthor, row):
+    row[namePrefix + "_email"] = uObj.email
+    row[namePrefix + "_name"] = uObj.name
+
+def getGitCommitData(namePrefix, cObj:GitCommit, row):
+    row[namePrefix + "_message"] = cObj.message
+
+    getGitUserData(namePrefix + "_author", cObj.author, row)
+
+def getFileData(namePrefix, fObj, row):
+    row[namePrefix + "_apiURL"] = fObj.contents_url
+    row[namePrefix + "_filename"] = fObj.filename
+    row[namePrefix + "_prefiousFilename"] = fObj.previous_filename
+    row[namePrefix + "_sha"] = fObj.sha
+    row[namePrefix + "_patch"] = fObj.patch
+    row[namePrefix + "_additions"] = fObj.additions
+    row[namePrefix + "_deletions"] = fObj.deletions
+    row[namePrefix + "_changes"] = fObj.changes
+    row[namePrefix + "_status"] = fObj.status
+    
 
 
-def getCommitData(namePrefix, cObj, g, row):
-    _author = cObj.author
-    _committer = cObj.committer
+def getCommitData(namePrefix, cObj:Commit, g, row):
     _stats = cObj.stats
     _files = cObj.files
 
-    print(_committer)
+    # if(len(_files) > 1):
+    #     print("MORE THAN 1 FILE", namePrefix, len(_files))
 
-    # commit author stats
-    #https://pygithub.readthedocs.io/en/latest/github_objects/NamedUser.html#github.NamedUser.NamedUser.collaborators
-    if(_author != None):
-        authorPrefix = namePrefix + "_author"
-        getUserData(authorPrefix, _author, row)
+    getFileData(namePrefix + "_file", _files[0], row)
 
-    if(_committer != None):
-        commiterPrefix = namePrefix + "_committer"
-        getUserData(commiterPrefix, _committer, row)
-
-    print(cObj.html_url)
+    getGitCommitData(namePrefix + "_git", cObj.commit, row)
 
 
     # General Commit stats
     row[namePrefix + "_totalAdditions"]  = _stats.additions
     row[namePrefix + "_totalDeletions"] = _stats.deletions
-    row[namePrefix + "_totalChanges"] = _stats.total
-
-    # Commit file stats
-
-
-def test(row):
-    row['repo'] = 'NO'
-
 
 def create_dataset(infilePath, g):
     pairDF = pd.read_csv(infilePath)
     
     noAuthor = 0
     noCommitter = 0
+    rows = []
     ## loop through all pairs
     #for i in range(pairDF.shape[0]):
-    for i in range(500):
-        pairRow = pairDF.iloc[i]
+    for i in range(10):
+        pairRow = pairDF.iloc[-i]
         dataRow = {}
-        try:
         
-            repo = g.get_repo(pairRow['repo'])
+        repo = g.get_repo(pairRow['repo'])
         
-            bug_commit = repo.get_commit(sha=pairRow['bug_hash'])
-            fix_commit = repo.get_commit(sha=pairRow['fix_hash'])
+        bug_commit = repo.get_commit(sha=pairRow['bug_hash'])
+        fix_commit = repo.get_commit(sha=pairRow['fix_hash'])
 
-        except:
-            continue
+        getCommitData("bug", bug_commit, g, dataRow)
+        getCommitData("fix", fix_commit, g, dataRow)
 
-        ## Seeing what portion of commits have github authors and/or commiters
-        if(bug_commit.author == None):
-            noAuthor += 1
-        if bug_commit.committer == None:
-            noCommitter += 1
+        rows.append(dataRow)
 
-        if fix_commit.author == None:
-            noAuthor += 1
-
-        if fix_commit.committer == None:
-            noCommitter += 1    
-            
-
-        #getCommitData("bug", bug_commit, g, dataRow)
-        #getCommitData("fix", fix_commit, g, dataRow)
 
         #print(dataRow)
+    dataSetDF = pd.DataFrame(rows)
 
-    print("AUTHOR STATS:")
-    print(noAuthor)
-    print(noCommitter)
-        #print(bug_commit)
-        #print(fix_commit)
+    return dataSetDF
 
+
+
+
+
+## For each row, get the info of the git repo in data/repos
+
+def getCommitInfo(repoObj, sha:str, cDict:dict, prefix:str):
+
+    print(repoObj.commit(sha).authored_datetime)
+    
     
 
 def main():
-    # if(len(sys.argv) != 3):
-    #     raise Exception("Not enough arguments, USAGE: python createDataset.py DATAFILE OUTPUTPATH")
+    if(len(sys.argv) != 3):
+        raise Exception("Not enough arguments, USAGE: python createDataset.py DATAFILE OUTPUTPATH")
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
     tokenFile = open(dir_path + "/gh_access_token.txt")
     access_token = tokenFile.readline().strip()
     tokenFile.close()
     
     inFile = sys.argv[1]
+    outFile = sys.argv[2]
 
     g = gh.Github(access_token)
 
     print("Github API request remaining:", g.get_rate_limit().core.remaining)
 
-    create_dataset(inFile, g)
+    df = create_dataset(inFile, g)
+
+    df.to_csv(outFile)
     
      
 
