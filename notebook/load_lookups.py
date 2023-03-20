6b3a59cafc555c2d3723a0b951a8b1421d3027f3
Brennan Wilkes <brennan@codexwilkes.com>
2023-03-19 17:08:12 -0700

    feat: Utilities for loading

--- notebook/load_lookups.py
+++ notebook/load_lookups.py
@@ -5,12 +5,15 @@ from Commit import CommitFactory
 Commit = CommitFactory()
 
 #TODO: Make this process more efficient @lucas
-def load_commit_lookup(pickle_dir = '../data/commit_lookups', verbose = True):
+def load_commit_lookup(pickle_dir = '../data/commit_lookups', verbose = True, max_commits = None, max_commit_bag_size = None):
 
     COMMIT_DATA_LOOKUP = {}
+
+    iterator = tqdm(os.listdir(pickle_dir))
     
     # loop through all the files in the directory
-    for file_name in tqdm(os.listdir(pickle_dir)):
+    for file_name in iterator:
+
         # check if the file is a pickle
         if file_name.endswith('.pickle'):
 
@@ -23,7 +26,15 @@ def load_commit_lookup(pickle_dir = '../data/commit_lookups', verbose = True):
             with open(pickle_file, 'rb') as f:
                 try:
                     data = pickle.load(f)
-                    COMMIT_DATA_LOOKUP.update(data)
+
+                    for sha in data:
+                        if max_commits != None and len(COMMIT_DATA_LOOKUP) > max_commits:
+                            iterator.close()
+                            return COMMIT_DATA_LOOKUP
+
+                        if(max_commit_bag_size == None or len(data[sha].bag_of_contexts) <= max_commit_bag_size):
+                            COMMIT_DATA_LOOKUP[sha] = data[sha]
+
                     if(verbose):
                         print("Appending pickle of length:", len(data.keys()), ", new dict length:", len(COMMIT_DATA_LOOKUP.keys()))
                 except Exception as e:
